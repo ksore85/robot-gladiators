5e90684ffb0942743ba6a5081df1c4780f1e26d8
ksore85 <84486015+ksore85@users.noreply.github.com>
2021-06-09 22:52:33 -0500

    blank null input

--- assets/js/game.js
+++ assets/js/game.js
@@ -81,6 +81,16 @@ var startGame = function() {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
 
+      // function to set name
+      var getPlayerName = function() {
+        var name = "";
+
+      while (name === "" name ==== null) {
+        name = prompt("What is your Robot's name?");
+      }
+        console.log("Your robot's name is" + name);
+      }
+
       // pick new enemy to fight based on the index of the enemyInfo array
       var pickedEnemyObj = enemyInfo[i];
 
